1a1ce1a6cb84fa49c88a9561042ce817fcb43654
minhthunguyen98 <bananashimshim@gmail.com>
2022-11-12 19:40:45 +0100

    add method ItemService

--- src/main/java/de/htwberlin/webtech/service/ItemService.java
+++ src/main/java/de/htwberlin/webtech/service/ItemService.java
@@ -3,6 +3,7 @@ package de.htwberlin.webtech.service;
 import de.htwberlin.webtech.persistence.ItemEntity;
 import de.htwberlin.webtech.persistence.ItemRepository;
 import de.htwberlin.webtech.web.api.Item;
+import de.htwberlin.webtech.web.api.ItemCreateRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,11 +20,22 @@ public class ItemService {
     public List<Item> findAll(){
         List<ItemEntity> items = itemRepository.findAll();
         return items.stream()
-                .map(itemEntity -> new Item(
-                        itemEntity.getId(),
-                        itemEntity.getItemName(),
-                        itemEntity.getUnit(),
-                        itemEntity.getQuantity()))
+                .map(this::transformEntity)
                 .collect(Collectors.toList());
+    }
+
+    public Item create(ItemCreateRequest request){
+        var itemEntity = new ItemEntity(request.getItemName(), request.getUnit(), request.getQuantity());
+        itemEntity = itemRepository.save(itemEntity);
+        return transformEntity(itemEntity);
+    }
+
+    private Item transformEntity(ItemEntity itemEntity){
+        return new Item(
+                itemEntity.getId(),
+                itemEntity.getItemName(),
+                itemEntity.getUnit(),
+                itemEntity.getQuantity()
+        );
     }
 }
