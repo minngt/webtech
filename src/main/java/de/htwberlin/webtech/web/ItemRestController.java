b469373e06754232f88fd57602823de195657ae0
minhthunguyen98 <bananashimshim@gmail.com>
2022-11-12 19:40:28 +0100

    add PostMapping

--- src/main/java/de/htwberlin/webtech/web/ItemRestController.java
+++ src/main/java/de/htwberlin/webtech/web/ItemRestController.java
@@ -1,13 +1,18 @@
 package de.htwberlin.webtech.web;
 
-import de.htwberlin.webtech.persistence.ItemRepository;
+
 import de.htwberlin.webtech.service.ItemService;
 import de.htwberlin.webtech.web.api.Item;
+import de.htwberlin.webtech.web.api.ItemCreateRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 @RestController
@@ -23,4 +28,12 @@ public class ItemRestController {
     public ResponseEntity<List<Item>> fetchItems(){
         return ResponseEntity.ok(itemService.findAll());
     }
+
+    @PostMapping(path = "/api/v1/items")
+    public  ResponseEntity<Void> createItem(@RequestBody ItemCreateRequest request) throws URISyntaxException {
+        var item = itemService.create(request);
+        URI uri = new URI("api/v1/items/" + item.getId());
+        return ResponseEntity.created(uri).build();
+    }
+
 }
